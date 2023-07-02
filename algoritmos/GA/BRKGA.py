0aff545e4967680637f8928eeb5f04dfe6a84dae
Willian Lemos <50865278+Williannlemoss@users.noreply.github.com>
2023-07-02 07:12:43 -0300

    finish dighe1

--- algoritmos/GA/BRKGA.py
+++ algoritmos/GA/BRKGA.py
@@ -141,7 +141,7 @@ def plotar(indiv, f):
     # plt.show()
     # plt.title("Tempo Requerido: {:.2f}".format(indiv.fitness.values[0]))
     # fig1.savefig(f'{f}.png', dpi=300)
-    fig1.savefig(f'resultados/pol9/pol910/{f}.png', dpi=300)
+    fig1.savefig(f'resultados/dighe1/dighe110/{f}.png', dpi=300)
     plt.close()
 
 # def genIndividuo(edges):
@@ -673,17 +673,17 @@ def preProcess(file):
 #10 execuções calcular desvio, media, pior e melhor
 files = [
     # 'fu',# esse OK
-    #'instance_01_2pol', #esse OK
-    #'instance_01_4pol', #esse OK
+    # 'instance_01_2pol', #esse OK
+    # 'instance_01_4pol', #esse OK
     # 'instance_01_5pol', #esse OK
-    'instance_01_9pol', #esse
+    # 'instance_01_9pol', #esse OK
+    'dighe1', #esse
     # 'albano',  #esse
     # 'blaz1',
     # 'blaz2',
     # 'blaz3',
     # 'dighe1',
     # 'dighe2',
-    # 'fu',# esse
 
     # 'instance_01_6pol',
     # 'instance_01_7pol',
@@ -776,7 +776,7 @@ if __name__ == "__main__":
                         # print("Inds: ", iteracao[4], file=file_write)
                         print(file=file_write)
                         dadosExel.append((iteracao[2][0].fitness.values[0], tempoExecution))
-                        plotar(iteracao[2][0], f"pol910" + str(k[0]) + ' ' + str(k[1]) + ' ' + str(k[2]))
+                        plotar(iteracao[2][0], f"dighe110" + str(k[0]) + ' ' + str(k[1]) + ' ' + str(k[2]))
                         fig1, f1_axes = plt.subplots(ncols=1, nrows=1, constrained_layout=True)
                         fig1.set_size_inches((10, 10))
                         gens, inds = iteracao[3], iteracao[4]
@@ -787,7 +787,7 @@ if __name__ == "__main__":
                         f1_axes.set_ylim(inds[-1] - 10, inds[0] + 10)
                         f1_axes.plot(gens, inds, color='blue')
                         plt.close()
-            workbook = openpyxl.load_workbook('pol9.xlsx')
+            workbook = openpyxl.load_workbook('dighe1.xlsx')
             sheet = workbook.active
             ultima_linha = sheet.max_row
             linhaExel = ultima_linha + 1
@@ -797,5 +797,5 @@ if __name__ == "__main__":
                 sheet.cell(row=linhaExel, column=colunaExel).value = dado[0]
                 colunaExel = colunaExel + 1
                 sheet.cell(row=linhaExel, column=colunaExel).value = dado[1]
-            workbook.save('pol9.xlsx')
+            workbook.save('dighe1.xlsx')
             exit(0)
